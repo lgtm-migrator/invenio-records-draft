feca087cfc2031609ab55a5df7f9077967447802
Mirek Simek <miroslav.simek@vscht.cz>
2019-12-18 23:03:31 +0100

    removed print

--- tests/conftest.py
+++ tests/conftest.py
@@ -81,10 +81,6 @@ def base_app():
     print('records draft registered to app')
     InvenioRecordsDraft(app_)
 
-    # invenio 3.2 sets _current_suffix to random number, so reset it back to empty
-    print("Invenio search current suffix", app_.extensions['invenio-search'].current_suffix,
-          app_.extensions['invenio-search'].mappings, app_.extensions['invenio-search'].aliases)
-
     return app_
 
 
